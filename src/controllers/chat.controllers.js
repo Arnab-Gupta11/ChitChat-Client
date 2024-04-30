33c47fbf2083b060c57d50281b2a75f70a890b78
Arnab Gupta <arnabgupta1111@gmail.com>
2024-04-30 18:29:20 +0600

    Fetching all chats of a particular user

--- src/controllers/chat.controllers.js
+++ src/controllers/chat.controllers.js
@@ -1,6 +1,9 @@
 import { Chat } from "../models/chat.model.js";
+import { User } from "../models/users.model.js";
 
-//access ono to one chat
+//create or access ono on one chat
+//method: POST
+//endpoint: http://localhost:5000/api/v1/chats
 const accessChat = async (req, res) => {
   try {
     const { userId } = req.body;
@@ -10,19 +13,21 @@ const accessChat = async (req, res) => {
       return res.status(400).json({ success: "Fail", message: "UserId params not sent with request" });
     }
 
+    //If there is chat exist between users
     var isChat = await Chat.find({
       isGroupChat: false,
       $and: [{ users: { $elemMatch: { $eq: req.user._id } } }, { users: { $elemMatch: { $eq: userId } } }],
     })
-      .populate("users", "-password")
+      .populate("users", "-password") //get all the data of users from Users collection without password.
       .populate({
         path: "latestMessage",
         populate: { path: "sender", select: "name profilePhoto email" },
       });
 
     if (isChat.length > 0) {
-      res.send(isChat[0]);
+      res.send(isChat[0]); //With one user there is only exist one chat so send the response of 1st index.
     } else {
+      //If there is no chat availabel beteween two user then create a chat.
       var chatData = {
         chatName: "sender",
         isGroupChat: false,
@@ -41,4 +46,26 @@ const accessChat = async (req, res) => {
   }
 };
 
-export { accessChat };
+//Fetch all chats of a particular user
+//method: GET
+//endpoint: http://localhost:5000/api/v1/chats
+const fetchChats = async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name profilePic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    console.log(`Error from fetchChats: ${error.message}`);
+  }
+};
+
+export { accessChat, fetchChats };
--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { accessChat } from "../controllers/chat.controllers.js";
+import { accessChat, fetchChats } from "../controllers/chat.controllers.js";
 import { verifyToken } from "../middleware/verifyToken.middleware.js";
 const router = express.Router();
 
-router.route("/").post(verifyToken, accessChat);
+router.route("/").post(verifyToken, accessChat).get(verifyToken, fetchChats);
 
 export default router;
